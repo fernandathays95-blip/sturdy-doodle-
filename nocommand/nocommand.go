facaf21a27442bce961618251aa16bbe39d0e76a
fernandathays95-blip <fernandathays95@gmail.com>
2025-11-07 22:20:27 -0300

    Create nocommand.go

---
+++ nocommand/nocommand.go
@@ -0,0 +1,286 @@
+package nocommand
+
+func main() {
+var 1 rune = 'without command'
+var 2 rune = 'sin mando'
+var 3 rune = 'sans commande'
+var 4 rune = 'ohne Befehl'
+var others rune = 'senza comando
+sem comando
+zonder commando
+без команды
+无指令
+コマンドなし
+명령 없이
+بدون أمر
+बिना कमांड
+komutsuz
+utan kommando
+uten kommando
+uden kommando
+χωρίς εντολή
+ไม่มีคำสั่ง
+không có lệnh
+bez polecenia
+bez příkazu
+parancs nélkül
+ilman komentoa
+בלי פקודה
+tanpa perintah
+zonder opdracht
+zonder bevel
+zonder richtlijn
+без указания
+без наказания
+без команди
+без командування
+без керування
+не маючи команду
+без інструкції
+не маючи наказу
+без команде
+без заповеди
+без припису
+без приказа
+無命令
+コマンドなしです
+명령이 없음
+คำสั่งไม่มี
+ไม่มีคำสั่ง
+कोई आदेश नहीं
+બિન આદેશ
+বিনা কমান্ড
+నిర্দেশం లేకుండా
+ಕಮಾಂಡ್ ಇಲ್ಲದೆ
+கமாண்ட் இல்லாமல்
+තහනම් කටයුතු
+មិនមានពាក្យបញ្ជា
+ਲਾਂਭਾ ਹੁਕਮ ਨਹੀਂ
+без директиви
+без команд
+без керування
+не маючи наказу
+не маючи команди
+non-command
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+ingen kommando
+без распоряжения
+без наставления
+без управления
+без директив
+без вказівки
+без наказу
+без команди
+без заповiдi
+без команде
+без приписа
+無指令
+コマンドなし
+명령 없음
+คำสั่งไม่มี
+ไม่มีคำสั่ง
+कोई आदेश नहीं
+બિન આદેશ
+বিনা কমান্ড
+నిర্দেশం లేకుండా
+ಕಮಾಂಡ್ ಇಲ್ಲದೆ
+கமாண்ட் இல்லாமல்
+තහනම් කටයුතු
+មិនមានពាក្យបញ្ជា
+ਲਾਂਭਾ ਹੁਕਮ ਨਹੀਂ
+tilldelningsfri
+utan kommando
+ilman komentoa
+sans directive
+ohne Anweisung
+senza direttiva
+sin directiva
+without directive
+zonder richtlijn
+zonder bevel
+zonder opdracht
+ingen direktiv
+ingen ordre
+ingen kommando
+ingen anvisning
+ingen direktiv
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordre
+ingen anvisning
+ingen direktiv
+ingen kommando
+ingen ordonans
+ingen anvisning
+ingen direktiv
+ingen befehl
+ingen kommando
+ingen directive
+ingen komando
+ingen ordonans
+ingen anvisning
+ingen direktiv
+ingen kommando
+ingen directive
+ingen komando
+ingen ordonans
+ingen anvisning
+ingen direktiv
+ingen komando
+ingen directive
+ingen befehl
+ingen ordonans
+ingen anvisning
+ingen direktiv
+ingen komando
+ingen directive
+ingen befehl
+ingen ordonans
+ingen anvisning
+ingen direktiv
+ingen kommando
+ingen direktiv
+ingen ordning
+ingen befehl
+ingen komando
+ingen anvisning
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen komando
+ingen direktiv
+ingen ordning
+ingen befehl
+ingen anvisning
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+ingen ordning
+ingen befehl
+ingen komando
+ingen direktiv
+ingen ordonans
+ingen directive
+'
+}
